e9d52dea6432a50c4b35bdeaed443e8f6176f70b
likanug <likanug.g@qq.com>
2026-01-24 18:24:30 +0800

    feat: add language switch selector in setting menu

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,28 @@
 import { memo } from 'react';
-import { Flex } from '@mantine/core';
+import { Select } from '@mantine/core';
+import { IconLanguage } from '@tabler/icons-react';
+import languages from '../locale/languages.ts';
+import { handleSettingChange } from '../utils/handleSettingChange.ts';
+import { DispatchType } from '../types/IEvents.ts';
+import { useTranslation } from 'react-i18next';
+import i18n from '../i18n.ts';
 
 function Settings() {
+  const { t } = useTranslation();
   return (
-    <Flex align={'center'} justify={'center'} direction={'column'} gap={'md'}>
-      "这是Settings页面"
-    </Flex>
+    <Select
+      leftSection={<IconLanguage />}
+      allowDeselect={false}
+      checkIconPosition={'right'}
+      my={'sm'}
+      label={t('Language')}
+      placeholder="Pick one"
+      // itemComponent={SelectItem}
+      data={languages}
+      maxDropdownHeight={400}
+      value={i18n.language}
+      onChange={(value) => handleSettingChange(DispatchType.ChangeAppLanguage, value as string)}
+    />
   );
 }
 
