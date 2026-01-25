580b4d2a930fb4d28ff74c7fe051928f45614ca0
likanug <likanug.g@qq.com>
2026-01-25 18:21:01 +0800

    feat: add app single-instance support

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,6 @@
+use tauri::Manager;
 use tauri_plugin_autostart::MacosLauncher;
+use tauri_plugin_dialog::{DialogExt, MessageDialogKind};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -13,6 +15,18 @@ const AUTOSTART_HIDDEN_ARG: &str = "--justtodo-autostart-hidden";
 pub fn run() {
     tauri::Builder::default()
         //.plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_single_instance::init(|app, _argv, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                window.show().unwrap();
+                window.set_focus().unwrap();
+            }else {
+                app.dialog()
+                    .message("app is running...")
+                    .kind(MessageDialogKind::Error)
+                    .title("Warning")
+                    .blocking_show();
+            }
+        }))
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             Some(vec![AUTOSTART_HIDDEN_ARG]), /* arbitrary number of args to pass to your app */
