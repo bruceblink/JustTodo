6041ad6534be3f5f16c1cd0f50cb9b01fce259fa
likanug <likanug.g@qq.com>
2026-01-25 16:31:26 +0800

    feat: backend support app autostart

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,22 @@
+use tauri_plugin_autostart::MacosLauncher;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+
+const AUTOSTART_HIDDEN_ARG: &str = "--justtodo-autostart-hidden";
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         //.plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_autostart::init(
+            MacosLauncher::LaunchAgent,
+            Some(vec![AUTOSTART_HIDDEN_ARG]), /* arbitrary number of args to pass to your app */
+        ))
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_log::Builder::default().build())
         .invoke_handler(tauri::generate_handler![greet])
